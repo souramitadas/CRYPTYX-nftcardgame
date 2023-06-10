b24d460eb554b86762c201aa54dcdbe431b23392
Souramita Das <souramitadas2306@gmail.com>
2023-06-10 23:34:46 +0530

    first commit

---
+++ client/src/contract/index.js
@@ -0,0 +1,4 @@
+import contract from './AVAXGods.json';
+export const ADDRESS = '0x98b4864B58DA2463A89D83A07de597E840D4B779'
+
+export const { abi: ABI } = contract;
---
+++ client/src/page/Home.jsx
@@ -0,0 +1,70 @@
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useGlobalContext } from '../context';
+import { PageHOC, CustomInput, CustomButton } from '../components';
+import { gameRules } from '../assets';
+
+const Home = () => {
+  const { contract, walletAddress, setShowAlert, gameData, setErrorMessage } = useGlobalContext();
+  const [playerName, setPlayerName] = useState('');
+  const navigate = useNavigate();
+
+  const handleClick = async () => {
+    try {
+
+      const playerExists = await contract.isPlayer(walletAddress);
+
+      if (!playerExists) {
+        await contract.registerPlayer(playerName, playerName);
+
+        setShowAlert({
+          status: true,
+          type: 'info',
+          message: `${playerName} is being summoned!`
+        });
+      }
+    } catch (error) {
+      setErrorMessage(error);
+    }
+  }
+  
+  useEffect(() => {
+    const checkForPlayerToken = async () => {
+      const playerExists = await contract.isPlayer(walletAddress);
+      const playerTokenExists = await contract.isPlayerToken(walletAddress);
+
+      if(playerExists && playerTokenExists) navigate('/create-battle')
+    }
+
+    if (contract) checkForPlayerToken();
+  }, [contract]);
+  
+  useEffect(() => {
+    if (gameData.activeBattle) {
+      navigate(`/battle/${gameData.activeBattle.name}`)
+    }
+  }, [gameData]);
+
+  return (
+    <div className='flex flex-col' >
+      <CustomInput
+        label='Name'
+        placeholder='Enter your player name'
+        value={playerName}
+        handleValueChange={setPlayerName}
+      />
+
+      <CustomButton
+        title='Register'
+        handleClick={handleClick}
+        restStyles='mt-6'
+      />
+    </div>
+  )
+};
+
+export default PageHOC(
+    Home,
+    <>Welcome to Cryptyx <br /> a Trading Card Game</>,
+    <>Connect your wallet to start playing <br /> Battle your way to win </>
+);
